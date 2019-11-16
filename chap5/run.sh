75fd29549c338804cdf4978a5e192b92c0995460
神戸隆太 <kambe3141@gmail.com>
2019-11-16 18:35:38 +0900

    chap5

--- chap5/run.sh
+++ chap5/run.sh
@@ -5,10 +5,7 @@ ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
   head -1 tiger.grm.desc &&
   (
-    # FIXME: Hack to make the sort-order the same on Mac/Darwin as on Linux.
-    # FIXME: Luckily we don't use 'z' in our testcase file names...
-    files=$(ls ../testcases/*.tig | sed 's/-/z/g' | sort | sed -e 's/z/-/g')
-    for tigerSource in $files; do
+    for tigerSource in ../testcases/*.tig; do
       echo -- $tigerSource --
       cat $tigerSource
       echo --
@@ -16,5 +13,6 @@ ml-yacc tiger.grm &&
       echo
     done
   ) > run.actual.out &&
-  diff -U5 run.expected.out run.actual.out | tee run.diff.out && echo "good!"
+  diff -U5 run.expected.out run.actual.out | tee run.diff.out &&
+  echo "good!"
 echo status=$?
